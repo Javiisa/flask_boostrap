a350a70f12e63d68452b175d6bec8ebc93faa95c
charlotte <charlottediaz88@gmail.com>
2022-11-16 11:02:44 -0300

    conexion a base de datos

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app,db)
 
 class Message(db.Model):
+    __tablename__ = 'messages'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String (128), nullable=False)
     content = db.Column(db.Text, nullable=False)
@@ -17,14 +18,9 @@ class Message(db.Model):
     def __repr__(self):
         return f'<Message {self.title}>'
 
-messages = [{'title': 'Message One',
-             'content': 'Message One Content'},
-            {'title': 'Message Two',
-             'content': 'Message Two Content'}
-            ]
-
 @app.route('/')
 def index():
+    messages = Message.query.all()
     return render_template('index.html', messages = messages)
 
 @app.route('/create', methods =('GET', 'POST'))
@@ -38,7 +34,9 @@ def create():
         elif not content:
             flash('El contenido es obligatorio')
         else:
-            messages.append({'title': title, 'content': content})
+            messages = Message( 'title' : title, 'content': content)
+            db.session.add(message)
+            db.session.commit()
             return redirect(url_for('index'))
 
     return render_template('create.html')
