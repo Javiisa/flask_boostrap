b5cd8e79d3282e8e990bc02ff13e81d5a032be29
charlotte <charlottediaz88@gmail.com>
2022-11-25 11:06:08 -0300

    refactorizacion parte 1

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'cualquier cosa'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1968@localhost:5432/flask_bootstrap'
 
 db = SQLAlchemy(app)
 migrate = Migrate(app,db)
@@ -18,52 +16,6 @@ class Message(db.Model):
 
     def __repr__(self):
         return f'<Message {self.title}>'
-
-@app.route('/')
-def index():
-    messages = Message.query.all()
-    return render_template('index.html', messages = messages)
-
-@app.route('/create', methods =('GET', 'POST'))
-def create():
-    if request.method == 'POST':
-        title = request.form['title']
-        content = request.form['content']
-        picture = request.form['picture']
-
-        if not title:
-            flash('El titulo es obligatorio')
-        elif not content:
-            flash('El contenido es obligatorio')
-        else:
-            message = Message(title = title, content = content, picture = picture)
-            db.session.add(message)
-            db.session.commit()
-            return redirect(url_for('index'))
-
-    return render_template('create.html')
-
-
-@app.route('/<id>/update', methods = ('GET', 'POST'))
-def update(id):
-    message = Message.query.filter_by(id = id).first()
-    if request.method == 'POST':
-        if message:
-            message.title = request.form['title']
-            message.content = request.form['content']
-            message.picture = request.form['picture']
-            db.session.commit()
-            return redirect('/')
-    return render_template('update.html', message = message)
-
-@app.route('/delete', methods = ['POST'])
-def delete():
-    id = request.form['id']
-    message = Message.query.filter_by(id=id).first()
-    db.session.delete(message)
-    db.session.commit()
-    flash('Mensaje Eliminado')
-    return redirect('/')
 
 
 @app.route('/usuario/<name>')
