b4888dc8b06685eb5c15ffbc822921af61b4712f
Your Name <you@example.com>
2022-12-07 09:31:31 -0300

    autenticacion 2 parte

--- app/main/routes.py
+++ app/main/routes.py
@@ -5,4 +5,9 @@ from flask import render_template
 def index():
     return render_template('index.html')
 
+@bp.app_errorhandler(404)
+def page_not_found(error):
+    print(error)
+    return render_template('page_not_found.html'), 404    
+
       
